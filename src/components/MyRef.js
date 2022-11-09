05d719a4bf0fd4bea8602742eee7a0fe7482b1d3
sreelekha-2 <89189294+sreelekha-2@users.noreply.github.com>
2022-11-09 17:16:49 +0530

    1 file modified

--- src/components/MyRef.js
+++ src/components/MyRef.js
@@ -4,15 +4,22 @@ import { useState, useRef } from 'react';
 export default function MyRef() {
   const inputEle = useRef();
 
+  const select = useRef();
+
   const handler = () => {
     console.log(inputEle);
     inputEle.current.focus();
     inputEle.current.style.backgroundColor = 'red';
+    inputEle.current.value = 'hello';
+    select.current.value = '';
   };
 
   return (
     <>
       <input type="text" ref={inputEle} />
+      <select ref={select}>
+        <option>h</option>
+      </select>
       <button onClick={handler}>Click Me</button>
     </>
   );
